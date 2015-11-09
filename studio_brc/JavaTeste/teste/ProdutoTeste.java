fd02448e834005074b2d2d94b05b9334a5b10e5c
mauricioferraro <mauricio.ferraro@globo.com>
2015-11-09 13:34:12 -0200

    Criando classe teste produto

---
+++ studio_brc/JavaTeste/teste/ProdutoTeste.java
@@ -0,0 +1,61 @@
+package teste;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test;
+
+import Produto.Produto;
+
+import bruna.Util.HibernateUtil;
+
+public class ProdutoTeste {
+	private Session Sessao;
+	private Transaction transacao;
+	
+
+		public void abreConexao(){
+			
+			this.Sessao = HibernateUtil.getSession().getCurrentSession();
+			this.transacao = this.Sessao.beginTransaction();
+		
+		
+	}
+		
+		public void fechaConexao(){
+			this.transacao.commit();
+			try {
+				if(this.Sessao.isOpen()){
+					this.Sessao.close();
+				}
+					
+				
+			} catch (Exception e) {
+				System.out.println("deu problema para fehar conexao"+ e.getMessage());
+			}
+			
+		}
+		
+		@Test
+		public void salvarProdutoTeste(){
+			
+			Produto p = new Produto();
+			
+			p.setDescricao("Produto Teste");
+			p.setDataCadastro(new Date());
+			p.setEstoque(20);
+			p.setUnidade("quilo");
+			p.setValor(10.90f);
+			p.setId(1);
+			
+			abreConexao();
+			this.Sessao.save(p);
+			fechaConexao();
+			
+			assertNull(null);
+		}
+
+}
